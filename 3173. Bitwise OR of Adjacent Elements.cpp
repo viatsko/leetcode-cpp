7a3679c43e5ad941a8e3e027e4561574c18eb921
Valerii Iatsko <viatsko@viatsko.me>
2026-06-30 23:44:11 +0200

    Create 3173. Bitwise OR of Adjacent Elements.cpp

---
+++ 3173. Bitwise OR of Adjacent Elements.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    vector<int> orArray(vector<int>& nums) {
+        vector<int> ans;
+        ans.reserve(size(nums) - 1);
+        for (int i = 0; i < size(nums) - 1; i++) {
+            ans.push_back(nums[i] | nums[i + 1]);
+        }
+        return ans;
+    }
+};
