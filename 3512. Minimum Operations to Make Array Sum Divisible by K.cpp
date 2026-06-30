fa35be8fbc5418ada29818a4c1f2999420a240d8
Valerii Iatsko <viatsko@viatsko.me>
2026-07-01 00:22:38 +0200

    Create 3512. Minimum Operations to Make Array Sum Divisible by K.cpp

---
+++ 3512. Minimum Operations to Make Array Sum Divisible by K.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int k) {
+        int sum = 0;
+        for (auto & num : nums) {
+            sum += num;
+        }
+        return sum - sum / k * k;
+    }
+};
